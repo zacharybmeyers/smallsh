bac08479c9984204a2683801567f1c68bccda912
Zachary Meyers <meyersz@oregonstate.edu>
2022-01-25 09:59:19 -0800

    write prompt_user() and update filenames

--- smallsh.c
+++ smallsh.c
@@ -1,5 +1,22 @@
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
+
+void prompt_user() {
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread = 0;
+    
+    printf(": ");
+    nread = getline(&line, &len, stdin);
+    line[nread - 1] = '\0';
+    printf("you entered: %s\n", line);
+    
+    free(line);
+}
 
 int main() {
-    printf("here we go smallsh!\n");
+    printf("welcome to smallsh!\n");
+    prompt_user();
+    return 0;
 }
