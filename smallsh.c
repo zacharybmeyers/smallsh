1752a24c4ff7895c00efcf5f1cc58395abbd05ec
Zachary Meyers <meyersz@oregonstate.edu>
2022-01-26 12:34:42 -0800

    pid is saved to command line struct, memory leaks fixed

--- smallsh.c
+++ smallsh.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 typedef struct CommandLine {
     char *command;
@@ -9,6 +10,7 @@ typedef struct CommandLine {
     char *input_file;
     char *output_file;
     int background;
+    int pid;
 } CommandLine;
 
 // helper function to print contents of CommandLine struct
@@ -23,13 +25,21 @@ void debug_command_line(CommandLine *cmd) {
     printf("cmd->input_file: %s\n", cmd->input_file);
     printf("cmd->output_file: %s\n", cmd->output_file);
     printf("cmd->background: %d\n", cmd->background);
+    printf("cmd->pid: %d\n", cmd->pid);
 }
 
 // function takes a valid line and unpacks it into a CommandLine struct
-void create_command_line(char *line) {
+CommandLine *create_command_line(char *line) {
     CommandLine *cmd = malloc(sizeof(CommandLine));
-    // init background to false
+    // init struct fields
+    cmd->command = NULL;
+    for (int i = 0; i < 512; i++) {
+        cmd->args[i] = NULL;
+    }
+    cmd->input_file = NULL;
+    cmd->output_file = NULL;
     cmd->background = 0;
+    cmd->pid = 0;
     
     // start tokenizing line, make copy as prev_token for later use
     char *saveptr;
@@ -45,12 +55,7 @@ void create_command_line(char *line) {
     token = strtok_r(NULL, " ", &saveptr);
 
     // create flags to track special symbols and index of arguments
-    int input_flag = 0, output_flag = 0, i = 0;
-
-    // dummy pid
-    int pid = 39387;
-    char str_pid[256];
-    sprintf(str_pid, "%d", pid);    
+    int input_flag = 0, output_flag = 0, i = 0;  
 
     while (token != NULL) {        
         // save token as input file
@@ -76,20 +81,19 @@ void create_command_line(char *line) {
         }
         // otherwise, save token as an argument
         else {
-            // variable expansion of $$ into smallsh pid
+            // variable expansion: if $$ set smallsh pid
             if (strcmp(token, "$$") == 0) {
-                cmd->args[i] = calloc(strlen(str_pid) + 1, sizeof(char));
-                strcpy(cmd->args[i], str_pid);
-                i++;
-            } else {
-                cmd->args[i] = calloc(strlen(token) + 1, sizeof(char));
-                strcpy(cmd->args[i], token);
-                i++;  
-            }      
+                cmd->pid = getpid();
+            }
+            cmd->args[i] = calloc(strlen(token) + 1, sizeof(char));
+            strcpy(cmd->args[i], token);
+            i++;     
         }
 
         // store prev_token
-        prev_token = realloc(prev_token, sizeof(token));
+        // prev_token = realloc(prev_token, sizeof(token));
+        free(prev_token);
+        prev_token = calloc(strlen(token) + 1, sizeof(char));
         strcpy(prev_token, token);
 
         // move to next token
@@ -100,43 +104,69 @@ void create_command_line(char *line) {
     if (strcmp(prev_token, "&") == 0) {
         cmd->background = 1;
         // remove & as last argument
-        cmd->args[i - 1] = NULL;
+        free(cmd->args[i-1]); 
+        cmd->args[i-1] = NULL;
     }
 
-    debug_command_line(cmd);
+    free(prev_token);
+    return cmd;
 }
 
-void prompt_user() {
-    // set max prompt length
-    int prompt_len = 2048;
-    char line[prompt_len];
-    
-    // start prompt with colon
-    printf(": ");
-    fgets(line, prompt_len, stdin);
-    
-    // remove newline char
-    for (int i = 0; i < prompt_len; i++) {
-        if (line[i] == '\n') {
-            line[i] = '\0';
+void free_command_line(CommandLine *cmd) {
+    free(cmd->command);
+    for (int i = 0; i < 512; i++) {
+        if (cmd->args[i] == NULL) {
             break;
         }
+        free(cmd->args[i]);
     }
-
-    printf("you entered: %s\n", line);
-
-    // if line is empty or line is a comment, return
-    if (strcmp(line, "") == 0 || line[0] == '#') {
-        printf("no command!\n");
-        return;
-    }
-
-    // not empty, break command into pieces and store in struct
-    create_command_line(line);
+    free(cmd->input_file);
+    free(cmd->output_file);
+    free(cmd);
 }
 
 int main() {
     printf("welcome to smallsh!\n");
-    prompt_user();
+    
+    int runsh = 1;
+
+    do {
+        // set max prompt length
+        int prompt_len = 2048;
+        char line[prompt_len];
+        
+        // start prompt with colon
+        printf(": ");
+        fgets(line, prompt_len, stdin);
+        
+        // remove newline char
+        for (int i = 0; i < prompt_len; i++) {
+            if (line[i] == '\n') {
+                line[i] = '\0';
+                break;
+            }
+        }
+
+        printf("you entered: %s\n", line);
+
+        // if line is empty or line is a comment, return
+        if (strcmp(line, "") == 0 || line[0] == '#') {
+            printf("no command!\n");
+            continue;
+        }
+
+        // not empty, break command into pieces and store in struct
+        CommandLine *cmd = create_command_line(line);
+        debug_command_line(cmd);
+
+        // exit 
+        if (strcmp(cmd->command, "exit") == 0) {
+            runsh = 0;
+        }
+
+        free_command_line(cmd);
+
+    } while (runsh);
+
     return 0;
 }
